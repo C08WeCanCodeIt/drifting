0ddd54c588df64a1a4c581c087c318ee1318f01a
undertherose <xilinw@uw.edu>
2019-03-08 17:05:18 -0800

    EP done

--- src/ForumSubmission.js
+++ src/ForumSubmission.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Button, Form, FormControl } from 'react-bootstrap';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//import { Button, Form, FormControl, FormCheck } from 'react-bootstrap';
+//import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export default class ForumSubmission extends Component {
     // Set up a blank title and description input field
     constructor(props) {
         super(props);
         this.state = {
-            mood: "",
-            body: "",
-            type: "",
-            tags: ""
+            emotion: "",
+            exercise:"1",
+            body: ["", "", "", "", "", "", ""],
+            tags: "",
+            isPublic: ""
         };    
         //this.handleChange = this.handleChange.bind(this);
         //this.postBottle = this.postBottle.bind(this);
@@ -26,6 +29,17 @@ export default class ForumSubmission extends Component {
         this.setState(change);
     }
 
+    handleQuestion(event) {
+        let index = event.target.name;
+        let value = event.target.value;
+        console.log(index, value);
+        let change = this.state.body;
+        change[index] = value;
+        this.setState({body: change}, () => {
+            console.log("body", this.state.body);
+        });
+    }
+
     // When the user click submit, the value change on title and description 
     //onSubmit = e => {
     //    e.preventDefault();
@@ -37,13 +51,13 @@ export default class ForumSubmission extends Component {
     //};
 
     addBottle() {
-        console.log(this.state.type);
+        console.log(this.state.isPublic);
         let bottle = {
-            mood: this.state.mood,
+            emotion: this.state.emotion,
+            exercise: this.state.exercise,
             body: this.state.body,
-            type: this.state.type,
             tags: this.state.tags,
-                
+            isPublic: this.state.isPublic,    
         }
         console.log("bottle", bottle);
         //this.cardsRef.push(card);    
@@ -51,10 +65,11 @@ export default class ForumSubmission extends Component {
 
     clearState() {
         this.setState({
-            mood: "",
-            body: "",
-            type: "",
-            tags: ""}, () => {
+            emotion: "",
+            exercise:"",
+            body: ["", "", "", "", "", "", ""],
+            tags: "",
+            isPublic: ""}, () => {
             console.log("empty", this.state);
         });
         
@@ -63,7 +78,7 @@ export default class ForumSubmission extends Component {
     postBottle() {
         //this.setState({type: "public"});
         this.setState(
-            {type: "public"}, 
+            {isPublic: true}, 
             () => {
                 console.log("post", this.state);
                 this.addBottle();
@@ -74,7 +89,7 @@ export default class ForumSubmission extends Component {
 
     saveBottle() {
         this.setState(
-            {type: "private"}, 
+            {isPublic: false}, 
             () => {
                 console.log("save", this.state);
                 this.addBottle();
@@ -96,52 +111,141 @@ export default class ForumSubmission extends Component {
                     <h2>How are you feeling right now?</h2>
                 </div>
                 <form>
-                    <div className="radio">
-                        <label>
-                            <input type="radio" 
-                                name="mood"
-                                value="happy"
-                                checked={this.state.mood==="happy"}
-                                onChange={(event) => { this.handleChange(event) }}
-                            />
-                        Happy
+                    <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label className="btn btn-secondary">
+                            <input type="radio"
+                                name="emotion"
+                                value="+2"
+                                checked={this.state.emotion==="+2"}
+                                onChange={(event) => { this.handleChange(event) }} 
+                            /> 
+                            Great
                         </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input type="radio" 
-                                name="mood"
-                                value="bad"
-                                checked={this.state.mood==="bad"}
-                                onChange={(event) => { this.handleChange(event) }}
-                            />
-                        Bad
+                        <label className="btn btn-secondary">
+                            <input type="radio"
+                                name="emotion"
+                                value="+1"
+                                checked={this.state.emotion==="+1"}
+                                onChange={(event) => { this.handleChange(event) }} 
+                            /> 
+                            Good
                         </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input type="radio" 
-                                name="mood"
-                                value="awful"
-                                checked={this.state.mood==="awful"}
-                                onChange={(event) => { this.handleChange(event) }}
-                            />
-                        Awful
+                        <label className="btn btn-secondary">
+                            <input type="radio"
+                                name="emotion"
+                                value="0"
+                                checked={this.state.emotion==="0"}
+                                onChange={(event) => { this.handleChange(event) }} 
+                            /> 
+                            Okay
+                        </label>
+                        <label className="btn btn-secondary">
+                            <input type="radio"
+                                name="emotion"
+                                value="-1"
+                                checked={this.state.emotion==="-1"}
+                                onChange={(event) => { this.handleChange(event) }} 
+                            /> 
+                            Bad
+                        </label>
+                        <label className="btn btn-secondary">
+                            <input type="radio"
+                                name="emotion"
+                                value="-2"
+                                checked={this.state.emotion==="-2"}
+                                onChange={(event) => { this.handleChange(event) }} 
+                            /> 
+                            Awful
                         </label>
                     </div>
 
                     <div className="form-group">
-                      <label htmlFor="exampleFormControlTextarea1">Emotional Processing</label>
+                      <label htmlFor="exampleFormControlTextarea1">What happend?</label>
                       <textarea className="form-control"
-                        name="body"
-                        value={this.state.body}
-                        onChange={(event) => { this.handleChange(event) }} 
+                        name="0"
+                        value={this.state.body[0]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
                         id="exampleFormControlTextarea1" 
                         rows="3"
                         placeholder="What happened?"
                         aria-label="some description texts">
                       </textarea>
                     </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">How are you feeling?</label>
+                      <textarea className="form-control"
+                        name="1"
+                        value={this.state.body[1]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">Can the situation be worse? How?</label>
+                      <textarea className="form-control"
+                        name="2"
+                        value={this.state.body[2]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">What are some factors that contributed to the situation?</label>
+                      <textarea className="form-control"
+                        name="3"
+                        value={this.state.body[3]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">What factors in the situation is in your control?</label>
+                      <textarea className="form-control"
+                        name="4"
+                        value={this.state.body[4]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">Can you brainstorm solutions you can do to address your situation?</label>
+                      <textarea className="form-control"
+                        name="5"
+                        value={this.state.body[5]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">How do you feel now?</label>
+                      <textarea className="form-control"
+                        name="6"
+                        value={this.state.body[6]}
+                        onChange={(event) => { this.handleQuestion(event) }} 
+                        id="exampleFormControlTextarea1" 
+                        rows="3"
+                        aria-label="some description texts">
+                      </textarea>
+                    </div>
+
+
+
                     <div className="form-group">
                       <label htmlFor="formGroupExampleInput">Tags</label>
                       <input type="text" className="form-control" 
@@ -155,15 +259,16 @@ export default class ForumSubmission extends Component {
                     </div>
                 </form>
                 <button className="btn btn-primary mr-2" onClick={() => this.postBottle()}>
-                    Post
+                    Public
                 </button>
                 <button className="btn btn-primary mr-2" onClick={() => this.saveBottle()}>
-                    Save
+                    Only I Can See
                 </button>
                 <button className="btn btn-primary mr-2" onClick={() => this.disposeBottle()}>
                     Dispose
                 </button>
             </div>
+
         );
     }
 }

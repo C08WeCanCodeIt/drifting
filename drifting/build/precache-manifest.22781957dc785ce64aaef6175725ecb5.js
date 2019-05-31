61d2f7b47b6e964de79a7cd11163e71eaa4ff2f7
chiuy5 <chiuy5@uw.edu>
2019-05-31 10:21:48 -0700

    redid build

---
+++ drifting/build/precache-manifest.22781957dc785ce64aaef6175725ecb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa93377642e29ed3d5a67ae4c8d70d4",
+    "url": "/drifting/drifting/build/index.html"
+  },
+  {
+    "revision": "6844158a116f9425e8a7",
+    "url": "/drifting/drifting/build/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b5f1492252b91651bbe3",
+    "url": "/drifting/drifting/build/static/css/main.f289913c.chunk.css"
+  },
+  {
+    "revision": "6844158a116f9425e8a7",
+    "url": "/drifting/drifting/build/static/js/2.aadcf451.chunk.js"
+  },
+  {
+    "revision": "b5f1492252b91651bbe3",
+    "url": "/drifting/drifting/build/static/js/main.2b5e3085.chunk.js"
+  },
+  {
+    "revision": "29496254940693751466",
+    "url": "/drifting/drifting/build/static/js/runtime~main.3f2069a9.js"
+  }
+]);

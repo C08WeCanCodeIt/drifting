501e2deaa3c146a97473296c51b54adc5fab6ec8
TheaTai <thea330@uw.edu>
2019-06-02 14:28:15 -0700

    Added terms and conditions

--- drifting/src/App.js
+++ drifting/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 /* import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Button, Form, FormControl, Container, Image } from 'react-bootstrap'; */
 //import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { HashRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 import ForumSubmission from "./pages/ForumSubmission";
@@ -16,6 +16,7 @@ import Exercise from "./pages/Exercise";
 import Support from "./pages/Support";
 import Crisis from "./pages/Crisis";
 import Gratitude from "./pages/Gratitude";
+import TermsConditions from "./pages/TermsConditions";
 
 
 import {EncShare, EncDispose, EPShare, GradShare} from "./pages/Results";
@@ -50,11 +51,14 @@ class App extends Component {
             <Route path="/encourage-share" component={EncShare} />
             <Route path="/processing-result" component={EPShare} />
             <Route path="/gratitude-result" component={GradShare} />
+            <Route path="/TermsConditions" component={TermsConditions} />
 
           </Switch>
+          <footer className="disclaimer">* Drifting was created for educational purposes, our team are not certified mental health professsionals<br/><Link to="/TermsConditions">Terms and Conditions</Link>
+          </footer>
+      
         </Router>
-        <footer className="disclaimer">* Drifting was created for educational purposes, our team are not certified mental health professsionals<br/>Terms and Conditions</footer>
-      </div >
+        </div >
 
     );
   }
